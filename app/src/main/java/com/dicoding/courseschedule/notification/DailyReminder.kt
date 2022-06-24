1c9c0dc218ab96c40fc0d7e47c883058dd40f9a7
DR_Muzi <dimasrafimuzi@gmail.com>
2022-06-24 14:52:56 +0700

    Initial Commit

--- app/src/main/java/com/dicoding/courseschedule/notification/DailyReminder.kt
+++ app/src/main/java/com/dicoding/courseschedule/notification/DailyReminder.kt
@@ -1,15 +1,34 @@
 package com.dicoding.courseschedule.notification
 
+import android.app.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
 import com.dicoding.courseschedule.data.DataRepository
+import com.dicoding.courseschedule.util.FunctionLibrary
 import com.dicoding.courseschedule.util.executeThread
+import java.util.*
 
 class DailyReminder : BroadcastReceiver() {
+    companion object {
+        private const val ID_REPEATING = 201
+        private const val NOTIFICATION_CHANNEL_ID_NUM = 202
+        private const val NOTIFICATION_CHANNEL_ID = "notification_daily_schedule"
+        private const val NOTIFICATION_CHANNEL_NAME = "daily-schedule"
+        @Volatile
+        private var instance: DailyReminder? = null
+        fun getInstance() : DailyReminder{
+            return synchronized(this){
+                instance ?: DailyReminder()
+            }
+        }
+    }
 
     override fun onReceive(context: Context, intent: Intent) {
         executeThread {
@@ -24,20 +43,76 @@ class DailyReminder : BroadcastReceiver() {
 
     //TODO 12 : Implement daily reminder for every 06.00 a.m using AlarmManager
     fun setDailyReminder(context: Context) {
+        val time = "06:00"
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, DailyReminder::class.java)
 
+        // schedule daily alarm
+        val timeArray = time.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]))
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]))
+        calendar.set(Calendar.SECOND, 0)
+
+        val pendingIntent = PendingIntent.getBroadcast(context, ID_REPEATING, intent, 0)
+
+        // atur interval alarm adalah per hari
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
+
+        FunctionLibrary.showToast(context, "Daily schedule notification set up")
     }
 
     fun cancelAlarm(context: Context) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, DailyReminder::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, ID_REPEATING, intent, 0)
 
+        alarmManager.cancel(pendingIntent)
     }
 
-    private fun showNotification(context: Context, content: List<Course>) {
-        //TODO 13 : Show today schedules in inbox style notification & open HomeActivity when notification tapped
+    fun showNotification(context: Context, content: List<Course>) {
+        // set notification inbox style
         val notificationStyle = NotificationCompat.InboxStyle()
         val timeString = context.resources.getString(R.string.notification_message_format)
         content.forEach {
             val courseData = String.format(timeString, it.startTime, it.endTime, it.courseName)
             notificationStyle.addLine(courseData)
         }
+        notificationStyle.setSummaryText("+${content.size} more")
+
+        // notification builder
+        val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(context.resources.getString(R.string.today_schedule))
+            .setContentText("inbox style notification")
+            .setStyle(notificationStyle)
+            .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+            .setSound(alarmSound)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+
+        // notification manager
+        val notificationManagerCompat = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        // Untuk android Oreo ke atas perlu menambahkan notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            /* Create or update. */
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT)
+
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
+            builder.setChannelId(NOTIFICATION_CHANNEL_ID)
+            notificationManagerCompat.createNotificationChannel(channel)
+        }
+
+        // build and show notification
+        val notification = builder.build()
+        notification.flags = Notification.FLAG_AUTO_CANCEL or Notification.FLAG_ONGOING_EVENT
+        notificationManagerCompat.notify(NOTIFICATION_CHANNEL_ID_NUM, notification)
     }
 }
